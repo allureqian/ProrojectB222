e2776b90266d40a53fce59648976781a52886a46
Allure <997190375@qq.com>
2016-06-13 09:46:14 +0800

    提交了一个东西

---
+++ ProrojectB111/Project111/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Project111
+//
+//  Created by Allure on 16/6/13.
+//  Copyright © 2016年 kawayi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
